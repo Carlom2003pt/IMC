48080fc4e82804a1d4fd33b884e3664d7e3eb65d
Carlom2003pt <carlom634@gmail.com>
2022-03-05 22:10:49 +0000

    IMC

---
+++ CalcularIMC/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace CalcularIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPeso = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxAltura = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCalcular
+            // 
+            this.buttonCalcular.ForeColor = System.Drawing.Color.Red;
+            this.buttonCalcular.Location = new System.Drawing.Point(249, 287);
+            this.buttonCalcular.Name = "buttonCalcular";
+            this.buttonCalcular.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalcular.TabIndex = 0;
+            this.buttonCalcular.Text = "Calcular";
+            this.buttonCalcular.UseVisualStyleBackColor = true;
+            this.buttonCalcular.Click += new System.EventHandler(this.buttonCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(67, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Peso";
+            // 
+            // textBoxPeso
+            // 
+            this.textBoxPeso.Location = new System.Drawing.Point(108, 48);
+            this.textBoxPeso.Name = "textBoxPeso";
+            this.textBoxPeso.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPeso.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(67, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Altura";
+            // 
+            // textBoxAltura
+            // 
+            this.textBoxAltura.Location = new System.Drawing.Point(108, 122);
+            this.textBoxAltura.Name = "textBoxAltura";
+            this.textBoxAltura.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAltura.TabIndex = 5;
+            this.textBoxAltura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxAltura_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(224, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ex:1,88";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.ForeColor = System.Drawing.Color.Red;
+            this.buttonLimpar.Location = new System.Drawing.Point(249, 255);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(75, 23);
+            this.buttonLimpar.TabIndex = 8;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(131, 176);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(70, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Resultado";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.ForeColor = System.Drawing.Color.Red;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "English",
+            "Portugues",
+            "中文",
+            "España",
+            "عربى",
+            "русский"});
+            this.comboBox1.Location = new System.Drawing.Point(620, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.Text = "Language";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(73, 220);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(73, 221);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(73, 221);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 15;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(73, 221);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 16;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(73, 221);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 321);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxAltura);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPeso);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCalcular);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPeso;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxAltura;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonLimpar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ CalcularIMC/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace CalcularIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            label10.Visible = false;
+            label11.Visible = false;
+        }
+
+        private void Calcular()
+        {
+            double imc, altura, peso;
+            peso = double.Parse(textBoxPeso.Text);
+            altura = double.Parse(textBoxAltura.Text);
+            imc = peso / (altura * altura);
+            textBox1.Text = imc.ToString();
+            if (imc < 18.5)
+            {
+                label4.Text = "Abaixo do Peso";
+                label6.Text = "Under Weight";
+                label7.Text = "體重不足";
+                label8.Text = "Bajo peso";
+                label9.Text = "تحت الوزن";
+                label10.Text = "Под весом";
+
+            }
+
+            if ((imc > 18.5) && (imc < 25))
+            {
+                label4.Text = "Peso Ideal";
+                label6.Text = "Ideal Weight";
+                label7.Text = "理想體重";
+                label8.Text = "Peso ideal";
+                label9.Text = "الوزن المثالي";
+                label10.Text = "Идеальный вес";
+            }
+
+
+
+            if ((imc > 25.1) && (imc < 30))
+            {
+                label4.Text = "Levemente Acima do Peso";
+                label6.Text = "Slightly Overweight";
+                label7.Text = "有點超重";
+                label8.Text = "Un poco de sobrepeso";
+                label9.Text = "زيادة الوزن قليلا";
+                label10.Text = "Немного избыточный вес";
+            }
+
+
+
+            if ((imc > 30.1) && (imc < 35))
+            {
+                label4.Text = "Obesidade Grau I";
+                label6.Text = "Obesity Degree I";
+                label7.Text = "肥胖I級";
+                label8.Text = "Obesidad Grado I";
+                label9.Text = "السمنة الصف الأول";
+                label10.Text = "Ожирение I степени";
+            }
+
+
+
+            if ((imc > 35.1) && (imc < 40))
+            {
+                label4.Text = "Obesidade Grau II(Severa)";
+                label6.Text = "Obesity Degree II(Severe)";
+                label7.Text = "二級肥胖（嚴重）";
+                label8.Text = "Obesidad de grado II (severa)";
+                label9.Text = "السمنة من الدرجة الثانية (شديدة)";
+                label10.Text = "Степень ожирения II степени (тяжелая)";
+            }
+
+
+
+            if (imc > 40.1)
+            {
+                label4.Text = "Obesidade Grau III(Mórbida)";
+                label6.Text = "Obesity Degree III(Morbid)";
+                label7.Text = "三級肥胖（病態）";
+                label8.Text = "Obesidad de grado III (mórbido)";
+                label9.Text = "السمنة من الدرجة الثالثة (المرضية)";
+                label10.Text = "Степень ожирения III степени (болезненная)";
+            }
+            label6.Visible = false;
+
+            
+        }
+        private void buttonCalcular_Click(object sender, EventArgs e)
+        {
+            Calcular();
+            
+        }
+
+        private void buttonLimpar_Click(object sender, EventArgs e)
+        {
+            textBoxAltura.Text = "";
+            textBoxPeso.Text = "";
+            label4.Text = "";
+            textBox1.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label9.Text = "";
+            label10.Text = "";
+            label11.Text = "";
+
+
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: //English
+                    label1.Text = "Weight";
+                    label2.Text = "Height";
+                    label5.Text = "Result";
+                    buttonLimpar.Text = "Clean";
+                    buttonCalcular.Text = "Calculate";
+                    label4.Visible = false;
+                    label6.Visible = true;
+                    label7.Visible = false;
+                    label8.Visible = false;
+                    label9.Visible = false;
+                    label10.Visible = false;
+                    label11.Visible = false;
+                    break;
+                case 1: //Portuguese
+                    label1.Text = "Peso";
+                    label2.Text = "Altura";
+                    label5.Text = "Resultado";
+                    buttonLimpar.Text = "Limpar";
+                    buttonCalcular.Text = "Calcular";
+                    label4.Visible = true;
+                    label6.Visible = false;
+                    label7.Visible = false;
+                    break;
+                case 2: //Chinese
+                    label1.Text = "重量";
+                    label2.Text = "高度";
+                    label5.Text = "結果";
+                    buttonLimpar.Text = "清潔";
+                    buttonCalcular.Text = "計算";
+                    label4.Visible = false;
+                    label6.Visible = false;
+                    label7.Visible = true;
+                    label8.Visible = false;
+                    label9.Visible = false;
+                    label10.Visible = false;
+                    label11.Visible = false;
+                    break;
+                case 3: //Spanish
+                    label1.Text = "Peso";
+                    label2.Text = "Altura";
+                    label5.Text = "Resultado";
+                    buttonLimpar.Text = "Limpiar";
+                    buttonCalcular.Text = "Calcular";
+                    label4.Visible = false;
+                    label6.Visible = false;
+                    label7.Visible = false;
+                    label8.Visible = true;
+                    label9.Visible = false;
+                    label10.Visible = false;
+                    label11.Visible = false;
+                    break;
+                case 4: //Arabic
+                    label1.Text = "وزن";
+                    label2.Text = "ارتفاع";
+                    label5.Text = "نتيجة";
+                    buttonLimpar.Text = "لينظف";
+                    buttonCalcular.Text = "احسب";
+                    label4.Visible = false;
+                    label6.Visible = false;
+                    label7.Visible = false;
+                    label8.Visible = false;
+                    label9.Visible = true;
+                    label10.Visible = false;
+                    label11.Visible = false;
+                    break;
+                case 5: //Russian
+                    label1.Text = "вес";
+                    label2.Text = "рост";
+                    label5.Text = "результат";
+                    buttonLimpar.Text = "чистить";
+                    buttonCalcular.Text = "высчитывает";
+                    label4.Visible = false;
+                    label6.Visible = false;
+                    label7.Visible = false;
+                    label8.Visible = false;
+                    label9.Visible = false;
+                    label10.Visible = true;
+                    label11.Visible = false;
+                    break;
+            }
+        }
+
+        private void textBoxAltura_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+                Calcular();
+        }
+    }
+}
